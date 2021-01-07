7cbe47a2b5d8a22b04e09dbc3f85d11293dc3d70
pangao <pangao@tuhu.cn>
2021-01-07 14:26:18 +0800

    修改名称

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-// import pfTable from './src/components/pf-table/pf-table'
-// import pFrom from './src/components/pf-table/p-form'
-// import pDialogForm from './src/components/pf-table/p-dialog-form'
-// import pfTabTable from './src/components/pf-table/pf-tab-table'
-// import pTable from './src/components/pf-table/p-table'
+
 import PlTable from './src/components/table'
-import PSearchTable from './src/components/search-table'
 import PlInput from './src/components/input'
 import PlSelect from './src/components/select'
 import PlRadio from './src/components/radio'
 import PlCheckbox from './src/components/checkbox'
 import PlDate from './src/components/date'
-import PlButton, { PlActionBtn } from './src/components/button'
+import PlButton from './src/components/button'
 import PlTipButton from './src/components/tip-button'
 import PlSwitch from './src/components/switch'
 import PlTime from './src/components/time'
@@ -18,13 +13,13 @@ import PlForm from './src/components/form'
 import PlSearchList from './src/components/search-list'
 import PlSearchTabList from './src/components/search-tab-list'
 import PlDialog from './src/components/dialog'
-import * as utils from './src/utils'
-import './src/styles/index.styl'
+import PlDialogFrom from './src/components/dialog-form'
+import * as utils from './utils'
+import './styles/index.styl'
 
 const PElement = {
   install: function (Vue, opts = {}) {
     Vue.component('pl-table', PlTable)
-    Vue.component('p-search-table', PSearchTable)
     Vue.component('pl-input', PlInput)
     Vue.component('pl-select', PlSelect)
     Vue.component('pl-radio', PlRadio)
@@ -35,15 +30,16 @@ const PElement = {
     Vue.component('pl-form', PlForm)
     Vue.component('pl-button', PlButton)
     Vue.component('pl-tip-button', PlTipButton)
-    Vue.component('pl-action-btn', PlActionBtn)
     Vue.component('pl-search-list', PlSearchList)
     Vue.component('pl-search-tab-list', PlSearchTabList)
     Vue.component('pl-dialog', PlDialog)
+    Vue.component('pl-dialog-form', PlDialogFrom)
     Vue.prototype.$PlElement = {
       tableConfig: opts.tableConfig,
       pageConfig: opts.pageConfig,
       selectConfig: opts.selectConfig,
       inputConfig: opts.inputConfig,
+      dateConfig: opts.dateConfig,
       formConfig: opts.formConfig
     }
   }
